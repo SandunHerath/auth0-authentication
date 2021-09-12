8c4e9ebaee7525874dd6fb8b5123c7198b80d7ac
Sandun Herath <sandunherath124@gmail.com>
2021-09-12 08:24:51 +0530

    ant design base added

--- auth0-new/src/App.js
+++ auth0-new/src/App.js
@@ -1,62 +1,21 @@
 import "./App.css";
-
-import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LoginTest from "./Login Test";
+import Home from "./AppHome";
 function App() {
-  const {
-    loginWithPopup,
-    loginWithRedirect,
-    logout,
-    user,
-    isAuthenticated,
-    getAccessTokenSilently,
-  } = useAuth0();
-  async function CallApi() {
-    try {
-      const res = await axios.get("http://localhost:5000/");
-      console.log(res);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  async function CallProtectedApi() {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await axios.get("http://localhost:5000/protected", {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
   return (
-    <div className="App">
-      <h2>Auth0 authentication</h2>
-      <ul>
-        <li>
-          <button onClick={loginWithPopup}>LoginWithPopup</button>
-        </li>
-        <li>
-          <button onClick={loginWithRedirect}>loginWithRedirect</button>
-        </li>
-        <li>
-          <button onClick={logout}>logout</button>
-        </li>
-        <h3>User is {isAuthenticated ? "Logged in" : "Not Logged in"}</h3>
-        {isAuthenticated && (
-          <pre style={{ textAlign: "start" }}>
-            {JSON.stringify(user, null, 2)}
-          </pre>
-        )}
-      </ul>
-
-      <button onClick={CallApi}>Call Api</button>
-      <button onClick={CallProtectedApi}>Call Protected Api</button>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/a">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <LoginTest />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
