ff8eafeee9fd4169b56c08a5f60d921975d65f8e
Sandun Herath <sandunherath124@gmail.com>
2021-09-22 01:13:18 +0530

    management token got

--- back/index.js
+++ back/index.js
@@ -4,6 +4,7 @@ var jwt = require("express-jwt");
 var jwks = require("jwks-rsa");
 const cors = require("cors");
 const axios = require("axios");
+var request = require("request");
 app.use(cors());
 
 var port = process.env.PORT || 5000;
@@ -13,18 +14,18 @@ app.post("/register", function (req, res) {
   res.send("got it");
 });
 
-// var jwtCheck = jwt({
-//   secret: jwks.expressJwtSecret({
-//     cache: true,
-//     rateLimit: true,
-//     jwksRequestsPerMinute: 5,
-//     jwksUri: "https://dev-zgsgae4p.us.auth0.com/.well-known/jwks.json",
-//   }),
-//   audience: "sandunherath124@gmail.com",
-//   issuer: "https://dev-zgsgae4p.us.auth0.com/",
-//   algorithms: ["RS256"],
-// }).unless({ path: ["/"] });
-// app.use(jwtCheck);
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-43-hfsb7.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https://dev-43-hfsb7.us.auth0.com/api/v2/",
+  issuer: "https://dev-43-hfsb7.us.auth0.com/",
+  algorithms: ["RS256"],
+}).unless({ path: ["/"] });
+app.use(jwtCheck);
 
 app.get("/", function (req, res) {
   res.send("Hello normal route");
@@ -34,7 +35,7 @@ app.get("/protected", async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const response = await axios.get(
-      "https://dev-zgsgae4p.us.auth0.com/userInfo",
+      "https://dev-43-hfsb7.us.auth0.com/userInfo",
       {
         headers: {
           authorization: `Bearer ${token}`,
@@ -44,6 +45,34 @@ app.get("/protected", async (req, res) => {
     console.log(token);
     const userInfo = response.data;
     res.send(userInfo);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/perm", async (req, res) => {
+  try {
+    const r = await axios.post(
+      "https://dev-43-hfsb7.us.auth0.com/oauth/token",
+      '{"client_id":"4I4BmJRqJZs4gnBmyZ2OK7U5RJrYEx9K","client_secret":"2_cDXD8OQhsl8T6zraI_Rjjf33Wch1gNc4LROrPG2RyQNrEWpVoZffpPGN-sNG6I","audience":"https://dev-43-hfsb7.us.auth0.com/api/v2/","grant_type":"client_credentials"}',
+      { headers: { "content-type": "application/json" } }
+    );
+    const token = r.data.access_token;
+  
+    try {
+      const res2 = await axios.get(
+        `https://dev-43-hfsb7.us.auth0.com/api/v2/users`,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(res2.data);
+      res.send(res2.data);
+    } catch (error) {
+      console.log(error.message);
+    }
   } catch (error) {
     console.log(error.message);
   }
