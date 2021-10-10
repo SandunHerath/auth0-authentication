7da7f810fffa8c0736c8f67c7c8ab7a2b9af1922
Sandun Herath <sandunherath124@gmail.com>
2021-10-10 23:00:49 +0530

    zoom api added

--- back/index.js
+++ back/index.js
@@ -101,7 +101,50 @@ app.get("/perm", async (req, res) => {
   }
 });
 
-//payhere
+//meeting
+app.post("/meeting", (req, res) => {
+  email = req.body.email;
+  var options = {
+    method: "POST",
+    uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
+    body: {
+      topic: "Meeting",
+      type: 1,
+      settings: {
+        host_video: "true",
+        participant_video: "true",
+      },
+    },
+    auth: {
+      bearer: token,
+    },
+    headers: {
+      "User-Agent": "Zoom-api-Jwt-Request",
+      "content-type": "application/json",
+    },
+    json: true, //Parse the JSON string in the response
+  };
+
+  rp(options)
+    .then(function (response) {
+      console.log("response is: ", response.join_url);
+      // response.status(200).json(response);
+      let dataRes = {
+        join_url: response.join_url,
+        meeting_password: response.encrypted_password,
+        meeting_id: response.id,
+      };
+      console.log("response: ", dataRes);
+      res.status(200).json(dataRes);
+
+      // res.send("create meeting result: " + JSON.stringify(response));
+    })
+    .catch(function (err) {
+      // API call failed...
+      console.log("API call failed, reason ", err);
+    });
+});
+
 
 
 app.use((req, res, next) => {
